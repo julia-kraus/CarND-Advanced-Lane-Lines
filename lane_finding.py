12ab8d305238c3c0549d6bb4bcb43865e5a06e9b
julia-kraus <j-kraus@gmx.de>
2019-02-17 23:53:18 +0100

    implement fit_polynomial()

--- lane_finding.py
+++ lane_finding.py
@@ -149,7 +149,23 @@ def search_lane_from_prior(binary_warped, left_fit, right_fit):
 
     return left_fitx, right_fitx, ploty
 
-#FITPOLYNOMIAL FUNCTION MISSING
+
+def fit_polynomial(img_shape, leftx, lefty, rightx, righty):
+    """Fits polynomials to lane line pixels"""
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img_shape[0]-1, img_shape[0])
+    # Calc both polynomials using ploty, left_fit and right_fit ###\n",
+    try:
+        left_fitx = left_fit[0] * ploty**2 + left_fit[1] * ploty + left_fit[2]
+        right_fitx = right_fit[0] * ploty**2 + right_fit[1] * ploty + right_fit[2]
+    except TypeError:
+        # Avoids an error if `left` and `right_fit` are still none or incorrect\n",
+        print('The function failed to fit a line!')
+        left_fitx = 1*ploty**2 + 1*ploty
+        right_fitx = 1*ploty**2 + 1*ploty
+    return left_fitx, right_fitx, ploty
 
 
 # this function MIGHT NOT BE not completely right yet. We need to figure out how to use ym_per_pix and xm_per_pix
