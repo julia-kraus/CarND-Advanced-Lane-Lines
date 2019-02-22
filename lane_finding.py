c4e5af4232dc71cbc3413623d0b1ebd7b821d848
julia-kraus <j-kraus@gmx.de>
2019-02-22 21:24:11 +0100

    new output images

--- lane_finding.py
+++ lane_finding.py
@@ -35,6 +35,23 @@ class Line:
         self.ally = None
 
 
+def hist(img):
+    # TO-DO: Grab only the bottom half of the image
+    # Lane lines are likely to be mostly vertical nearest to the car
+    # image shape is 720 - 1280
+    bottom_half = img[img.shape[0] // 2:, :]
+
+    # TO-DO: Sum across image pixels vertically - make sure to set `axis`
+    # i.e. the highest areas of vertical lines should be larger values
+    histogram = np.sum(bottom_half, axis=0)
+
+    plt.plot(histogram)
+    plt.show()
+    plt.savefig('histogram.png')
+
+    return histogram
+
+
 def search_lane_from_scratch(binary_warped):
     """Searches lane from binary warped image using histogram and sliding window technique.
     `out_img` return value only needed for plotting, not for further lane detection algorithm."""
@@ -111,7 +128,104 @@ def search_lane_from_scratch(binary_warped):
     left_fitx, right_fitx, ploty, left_fit, right_fit = fit_polynomial(binary_warped.shape, leftx, lefty, rightx,
                                                                        righty)
 
+
     return left_fitx, right_fitx, ploty, left_fit, right_fit
+
+
+def search_lane_from_scratch_plot(binary_warped):
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[binary_warped.shape[0] // 2:, :], axis=0)
+    # Create an output image to draw on and visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0] // 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # HYPERPARAMETERS
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+
+    # Set height of windows - based on nwindows above and image shape
+    window_height = np.int(binary_warped.shape[0] // nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated later for each window in nwindows
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        ### TO-DO: Find the four below boundaries of the window ###
+        win_xleft_low = leftx_current - margin  # Update this
+        win_xleft_high = leftx_current + margin  # Update this
+        win_xright_low = rightx_current - margin  # Update this
+        win_xright_high = rightx_current + margin  # Update this
+
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low),
+                      (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low),
+                      (win_xright_high, win_y_high), (0, 255, 0), 2)
+
+        ### TO-DO: Identify the nonzero pixels in x and y within the window ###
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & \
+                           (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices (previously was a list of lists of pixels)
+    try:
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+    except ValueError:
+        # Avoids an error if the above is not implemented fully
+        pass
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    left_fitx, right_fitx, ploty, left_fit, right_fit = fit_polynomial(binary_warped.shape, leftx, lefty, rightx,
+                                                                       righty)
+
+    ## Visualization ##
+    # Colors in the left and right lane regions
+    out_img[lefty, leftx] = [255, 0, 0]
+    out_img[righty, rightx] = [0, 0, 255]
+
+    # Plots the left and right polynomials on the lane lines
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+
+    plt.imshow(out_img)
+
+    return leftx, lefty, rightx, righty, out_img
 
 
 def search_lane_from_prior(binary_warped, left_fit, right_fit):
@@ -244,8 +358,8 @@ class LaneFinder:
         if self.left_lane.detected and self.right_lane.detected:
             # Search from prior
             left_fitx, right_fitx, ploty, left_fit, right_fit = search_lane_from_prior(binary_warped,
-                                                                  self.left_lane.best_fit,
-                                                                  self.right_lane.best_fit)
+                                                                                       self.left_lane.best_fit,
+                                                                                       self.right_lane.best_fit)
         else:
             # Search from scratch
             left_fitx, right_fitx, ploty, left_fit, right_fit = search_lane_from_scratch(binary_warped)
